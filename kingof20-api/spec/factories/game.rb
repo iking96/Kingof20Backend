d983eb4ed59dd463d4a08fb7acd39cab86121a50
Ishmael King <ishmaelrking@gmail.com>
2019-09-28 20:09:53 -0600

    Add delete to games controller

--- kingof20-api/spec/factories/game.rb
+++ kingof20-api/spec/factories/game.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :game do
     board { Array.new(Game.board_size) { Array.new(Game.board_size) { 0 } } }
     initiator_score { 0 }
     opponent_score { 0 }
-    initiator_rack { [1,2,3,4,5,6,7] }
-    opponent_rack { [1,2,3,4,5,6,7] }
+    initiator_rack { [1, 2, 3, 4, 5, 6, 7] }
+    opponent_rack { [1, 2, 3, 4, 5, 6, 7] }
     complete { false }
     available_tiles { Game.initial_available_tiles }
   end
