f956f7598818fc82457cbd21d1533c1682a1c2e1
Ishmael King <ishmaelrking@gmail.com>
2019-10-19 22:19:15 -0600

    Remove stale helper class and add specs

--- kingof20-api/app/services/play_logic/helpers.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module PlayLogic
-  class Helpers
-    class << self
-    end
-  end
-end
---
+++ kingof20-api/spec/services/play_logic/game_logic/game_helpers_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe(PlayLogic::GameLogic::GameHelpers) do
+  it "add some tests when ready!"
+end
