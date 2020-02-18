cc5ef361642994a2797c59ff9a80eadbe8f79086
Ishmael King <ishmaelrking@gmail.com>
2020-02-17 18:15:20 -0700

    Change frontend

---
+++ app/javascript/components/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import HelloWorld from './HelloWorld'
+
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => ("King of 20!")} />
+          <Route path="/hello" render={() => <HelloWorld greeting="potato"/>} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App
