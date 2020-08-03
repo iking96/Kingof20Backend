1b2ff4ee20167a2e99723c4b84709b6230306074
Ishmael King <ishmaelrking@gmail.com>
2020-08-02 21:55:43 -0700

    add cable to prod action cable url

--- app/javascript/frontend/utils/constants.js
+++ app/javascript/frontend/utils/constants.js
@@ -4,7 +4,7 @@ export const rackSize = 7
 const prod = {
   url: {
     API_URL: `http://54.69.119.37:3000`,
-    API_WS_ROOT: `ws://54.69.119.37:3000`
+    API_WS_ROOT: `ws://54.69.119.37:3000/cable`
   }
 };
 
