de17e49427366e971cb8b909244c05e7afb05221
Ishmael King <ishmaelrking@gmail.com>
2020-09-27 14:05:03 -0700

    spec: remove email requirement for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe(User, type: :model) do
   it { should validate_presence_of(:username) }
-  it { should validate_presence_of(:email) }
   it { should validate_presence_of(:encrypted_password) }
   it { should validate_uniqueness_of(:username) }
   it { should validate_uniqueness_of(:encrypted_password) }
